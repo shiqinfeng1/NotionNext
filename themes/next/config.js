2cc73b8942fb52dd92b8088a99fcdea655d9b7a1
shiqinfeng <shiqinfeng@jmail.com>
2023-10-13 16:03:56 +0800

    update theme config

--- themes/next/config.js
+++ themes/next/config.js
@@ -4,19 +4,21 @@ const CONFIG = {
   HOME_BANNER_Strings: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   NAV_TYPE: 'autoCollapse', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
-
-  POST_LIST_COVER: false, // 文章列表显示封面图
+  // 是否显示开始阅读按钮
+  SHOW_START_READING: true,
+  
+  POST_LIST_COVER: true, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
   POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
-  POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
+  POST_HEADER_IMAGE_VISIBLE: true, // 文章详情页是否显示封面图
 
   // 右侧组件
   RIGHT_BAR: true, // 是否显示右侧栏
   RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
-  RIGHT_AD: false, // 右侧广告
+  RIGHT_AD: true, // 右侧广告
 
   // 菜单
   MENU_HOME: true, // 显示首页
